81a1b518c8f61ec89affee6db1b6ec806cbfa5b5
Dantun1 <113800320+Dantun1@users.noreply.github.com>
2024-01-31 20:13:18 +0000

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,18 +1,22 @@
 <?php
 
+// This file handles the login process. It checks if the "submit" button has been pressed then runs the loginUser function, taking in the inputted
+// values as arguments. If the user has not entered anything, it shows an empty input error.
+// 
+
 if (isset($_POST["submit"])) {
-    $username = $_POST["uid"];
-    $pwd = $_POST["pwd"];
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
+
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
 
     if (emptyInputLogin($username,$pwd,) !== false){
         header("location: ../login.php?error=emptyinput");
         exit();
     }
     loginUser($conn, $username, $pwd);
-    echo '<div>This is a div element echoed from PHP.</div>';
 }
 else {
     header("location:../login.php");
