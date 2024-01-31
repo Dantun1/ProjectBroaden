fa04f497a627058572bb31946a5b0ea04eab31f0
Dantun1 <113800320+Dantun1@users.noreply.github.com>
2024-01-31 09:27:53 +0000

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,15 +1,14 @@
-const articleButtons = document.querySelectorAll('.liked-snippet-link');
+const snippetButtons = document.querySelectorAll('.liked-snippet-link');
+// Adds an event listener to every button in the liked/bookmarked post divs which redirects the user to the discovery page and places the article ID they wanted to view
+// at the front of the priority queue.
 
-articleButtons.forEach((button) => {
+snippetButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const snippetId = button.getAttribute('data-snippet-id');
     
-    // Use AJAX or fetch to add the snippetId to the session variable
-    // Example using fetch:
     fetch('add_snippetid.php?snippetId=' + snippetId, {
-      method: 'POST', // Use POST or GET as needed
+      method: 'POST',
     }).then(() => {
-      // Redirect to discover.php after adding to the session
       window.location.href = 'discover.php';
     });
   });
