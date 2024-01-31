8f5a840bea6511fead2e27fee0bdee466e7bfb7d
Dantun1 <113800320+Dantun1@users.noreply.github.com>
2024-01-31 20:13:01 +0000

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -3,6 +3,8 @@
 include_once 'dbh.inc.php';
 
 function emptyInputSignup($name,$email,$username,$pwd, $pwdrepeat) {
+    // Checks if any of the signup variables are empty. Returns true if one or more are empty, False if not.
+
     $result = false;
     if (empty($name) || empty($email) || empty($username) || empty($pwd) || empty($pwdrepeat)) {
         $result = true;
@@ -14,6 +16,8 @@ function emptyInputSignup($name,$email,$username,$pwd, $pwdrepeat) {
 }
 
 function invalidUid($username) {
+    // Checks that inputted username contains exclusively numbers or letters. Returns True if there are invalid characters, False if not.
+
     $result= false;
     if (!preg_match("/^[a-zA-Z0-9]*$/",$username)) {
         $result = true;
@@ -25,6 +29,8 @@ function invalidUid($username) {
 }
 
 function invalidEmail($email) {
+    // Checks that email is valid. Returns True if invalid, False if not. 
+
     $result= false;
     if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
         $result = true;
@@ -36,6 +42,8 @@ function invalidEmail($email) {
 }
 
 function pwdMatch($pwd,$pwdrepeat) {
+    // Checks that password and confirm password inputs are the same. Returns True if they don't match, False if not
+
     $result = false;
     if ($pwd !== $pwdrepeat){
         $result = true;
@@ -47,7 +55,9 @@ function pwdMatch($pwd,$pwdrepeat) {
 }
 
 function uidExists($conn,$username,$email) {
-    $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;"; //grabs user database
+    // Checks in database to see if username already exists. 
+
+    $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;"; 
     $stmt =  mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt,$sql)) {
         header("location: ../signup.php?error=stmtfailed");
@@ -57,7 +67,7 @@ function uidExists($conn,$username,$email) {
     mysqli_stmt_bind_param($stmt, "ss", $username, $email);
     mysqli_stmt_execute($stmt);
 
-    $resultData = mysqli_stmt_get_result($stmt); //gets back all usernames/emails which are duplicates
+    $resultData = mysqli_stmt_get_result($stmt); 
 
     if ($row = mysqli_fetch_assoc($resultData)) {
         return $row;
@@ -70,7 +80,9 @@ function uidExists($conn,$username,$email) {
 }
 
 function createUser($conn,$name,$email,$username,$pwd) {
-    $sql = "INSERT INTO users (usersName, usersEmail, usersUid, usersPwd) VALUES (?, ?, ?, ?);"; //inserts into databse
+    // Adds user information to database if all inputs are valid.
+
+    $sql = "INSERT INTO users (usersName, usersEmail, usersUid, usersPwd) VALUES (?, ?, ?, ?);";
     $stmt =  mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt,$sql)) {
         header("location: ../signup.php?error=stmtfailed");
@@ -87,6 +99,7 @@ function createUser($conn,$name,$email,$username,$pwd) {
 }
 
 function emptyInputLogin($username,$pwd) {
+    // Checks if user is missing any login inputs. Returns True if missing, False if not.
     $result = false;
     if (empty($username) || empty($pwd)) {
         $result = true;
@@ -98,6 +111,13 @@ function emptyInputLogin($username,$pwd) {
 }
 
 function loginUser($conn, $username, $pwd) {
+    // Checks if the username and password are valid by comparing the hashed password in the database to the inputted password.
+    // If the information is correct, a user session is created and the session variables are initialized.
+    // 
+    // The recommended snippet IDs are stored as an array in descending order of predicted rating as calculated by the recommender system.
+    // The current_index is stored as a pointer to the article being viewed and the user increments this in the discovery page by cycling back/forth
+    // The viewrequests represent the number of times a user has clicked on a previously liked/bookmarked article to view it.
+
     $uidExists = uidExists($conn,$username,$username); 
 
     if($uidExists === false) {
@@ -125,6 +145,8 @@ function loginUser($conn, $username, $pwd) {
 }
 
 function getPredictions(){
+    // Performs an api call to the Flask application running on port 8080. This application computes the predictions and returns the IDs as a JSON file.
+
     $userId = $_SESSION['userid'];
     $api = "http://localhost:8080/get_predictions?user_id=".$userId;
     $apiResponse = file_get_contents($api);
@@ -135,6 +157,8 @@ function getPredictions(){
 
 
 function getSnippetIds($conn){
+    // Function to get all snippet IDs from the database.
+
     $query = "SELECT snippetId FROM snippets;";
     $result = mysqli_query($conn, $query);
     $snippetIds = mysqli_fetch_all($result, MYSQLI_ASSOC);
@@ -144,6 +168,7 @@ function getSnippetIds($conn){
 }
 
 function getSnippetInfo($conn, $snippetId){
+    // Function to get all information about a snippet given its id.
     $query = "SELECT * FROM snippets WHERE snippetId =$snippetId;";
     $result = mysqli_query($conn, $query);
     $snippet = mysqli_fetch_assoc($result);
