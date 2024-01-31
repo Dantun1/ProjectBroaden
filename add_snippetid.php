c21ef28fa3a57f588593a7168790629b489907e8
Dantun1 <113800320+Dantun1@users.noreply.github.com>
2024-01-31 20:04:37 +0000

    Update add_snippetid.php

--- add_snippetid.php
+++ add_snippetid.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
+// File adds a snippet ID to the session array of articles when a user clicks on a button to view a specific article from their bookmarked/liked articles.
+
 $snippetId = $_GET['snippetId'];
 $_SESSION['snippetIds'][$_SESSION['current_index']+1] = $snippetId;
 $_SESSION['viewrequests'] ++;
-
-// Making so buttons add snippet id into snippetids variable.
-// Next post needs to go through priority queue and when it gets to end current index bigger than equal to count snippetids, say youve viewed all articles
-// if counter less than 0 do nothing no error.
